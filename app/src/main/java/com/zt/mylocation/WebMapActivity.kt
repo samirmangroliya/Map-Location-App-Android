046611eb0ed921ef2b7ae4088dafe8d8095cb5f6
Samir Mangroliya <sbmangroliya2012@gmail.com>
2021-10-19 14:23:41 +0530

    added location

--- app/src/main/java/com/zt/mylocation/WebMapActivity.kt
+++ app/src/main/java/com/zt/mylocation/WebMapActivity.kt
@@ -23,14 +23,17 @@ class WebMapActivity : AppCompatActivity() {
             val listLocation = mutableListOf<Location>()
             listLocation.add(Location("Select Location", 0.0, 0.0))
             listLocation.add(Location("Fullers", -30.01703, 153.13179))
+            listLocation.add(Location("Green Shed",-29.98904, 153.13602))
             listLocation.add(Location("Greys Road Farm", -29.9401, 153.12618))
             listLocation.add(Location("KD17", -30.00472, 153.13965))
+            listLocation.add(Location("Packing Shed", -29.98767, 153.14126))
             listLocation.add(Location("Sutton’s", -29.99817, 153.13941))
             listLocation.add(Location("S2 (86)", -29.98896, 153.14121))
             listLocation.add(Location("Section 1", -29.99267, 153.1442))
             listLocation.add(Location("Section 2", -29.99791, 153.15077))
             listLocation.add(Location("Section 3", -30.01207, 153.14249))
             listLocation.add(Location("Section 4", -29.99063, 153.13319))
+            listLocation.add(Location("Tols Main Maintenance Shed", -30.01577, 153.12422))
 
             val locationNameList = listLocation.map { it.name } as MutableList<*>
             val adapterLocation =
